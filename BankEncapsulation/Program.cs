89f95bd306f05794528f4b4bf1bfa074de8a5698
Drew Gregory <drewgregory@otmail.com>
2022-10-25 11:04:01 -0700

    attempt 1 completion

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var account = new BankAccount();
+
+            Console.WriteLine("How much would you like to deposit?");
+
+            var DepositAmount = double.Parse(Console.ReadLine());
+
+            account.Deposit(DepositAmount);
+
+            Console.WriteLine($"Your balance is now ${account.GetBalance()}");
         }
     }
 }
